99611bf754d59fc5811d80717b80ba650a9f78ad
Juniper Preston <unwttng@gmail.com>
2023-02-10 23:01:46 +0000

    Big move around

--- install_code.sh
+++ install_code.sh
@@ -1,11 +1,5 @@
 CIRCUITPY_VOLUME=/Volumes/CIRCUITPY
 
-# If --skip-libs is passed, skip copying the libraries
-SKIP_LIBS=0
-if [[ " $@ " =~ " --skip-libs " ]]; then
-    SKIP_LIBS=1
-fi
-
 if [ -d "$CIRCUITPY_VOLUME" ]; then
     echo "$CIRCUITPY_VOLUME is present, copying files..."
 else 
@@ -17,25 +11,13 @@ fi
 echo "Removing old files..."
 rm -f "$CIRCUITPY_VOLUME"/code.py
 rm -f "$CIRCUITPY_VOLUME"/main.py
-
-if [ $SKIP_LIBS -eq 0 ]; then
-    echo "  Removing libraries..."
-    rm -rf "$CIRCUITPY_VOLUME"/lib
-else
-    echo "  Skipping libraries..."
-fi
+rm -rf "$CIRCUITPY_VOLUME"/lib
 
 echo "Copying new files..."
 echo "  Copying main.py..."
 cp main.py "$CIRCUITPY_VOLUME/main.py"
 
-if [ $SKIP_LIBS -eq 0 ]; then
-    echo "  Copying Adafruit libraries..."
-    cp -r ext/adafruit "$CIRCUITPY_VOLUME/lib/"
-    echo "  Copying Pimoroni libraries..."
-    cp -r ext/pimoroni/pmk "$CIRCUITPY_VOLUME/lib/"
-else
-    echo "  Skipping libraries..."
-fi
+echo "  Copying lib..."
+cp -r lib "$CIRCUITPY_VOLUME/"
 
 echo "Done!"
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import board  # type: ignore
 import busio  # type: ignore
 import adafruit_ahtx0  # type: ignore
-import adafruit_logging as logging  # type: ignore
 
 from src.heater_controller.heater_controller_real import HeaterControllerReal
 from src.thermobox_controller import ThermoboxController
@@ -11,15 +10,22 @@ SCL = board.GP1
 i2c = busio.I2C(SCL, SDA)
 sensor = adafruit_ahtx0.AHTx0(i2c)
 
-logger = logging.getLogger("pi-pico-w-thermobox")
+
+class Logger:
+    def debug(self, message):
+        print("DEBUG:", message)
+
+    def info(self, message):
+        print("INFO:", message)
+
 
 heater_controller = HeaterControllerReal(board.LED, board.GP28)
 thermobox_controller = ThermoboxController(
     aht20_sensor=sensor,
     heater_controller=heater_controller,
-    logger=logger,
-    target_temperature=40.0,
-    temperature_wobble=0.1,
+    logger=Logger(),
+    target_temperature=26.0,
+    temperature_wobble=0.25,
 )
 
 thermobox_controller.run_forever()
